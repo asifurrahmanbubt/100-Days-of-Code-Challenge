9392b90132abd9b841fbe19713e02428fb1b4bb5
Asifur Rahman <122219223+asifurrahmanbubt@users.noreply.github.com>
2024-07-03 19:34:08 +0600

    Add files via upload

---
+++ Day 18/Factorial_Function.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Factorial_Function {
+    public static int factorial(int n){
+        int f = 1;
+        for(int i = 1; i <= n; i++){
+            f = f * i;
+        }
+        return f;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(factorial(n));
+    }
+}
