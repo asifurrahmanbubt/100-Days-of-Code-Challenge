27174990fc70efe2abf140994ccff23a14d5ee21
Asifur Rahman <122219223+asifurrahmanbubt@users.noreply.github.com>
2024-07-04 21:41:00 +0600

    Add files via upload

---
+++ Day 19/Divisors.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Divisors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        for (int i = 1; i <= A; i++) {
+            if (A % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
